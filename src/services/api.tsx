4ffaeb75840ca1b6ef358d525c4414dcdba12906
devBrigante <brennoreis411@gmail.com>
2025-02-14 16:38:04 -0300

    Removendo a pasta services do controle de versão e adicionando ao .gitignore

--- src/services/api.tsx
+++
@@ -1,7 +0,0 @@
-// https://api.themoviedb.org/3/movie/popular?api_key=10c61cf842a1dade1b2e54aee578d723&language=pt-BR&page=1
-
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL: "https://api.themoviedb.org/3/"
-})
